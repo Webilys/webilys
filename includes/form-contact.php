56c0c675a5167d7e32afd4a4cb1b51e6fd51f273
Priscilla DigiTech <priscilladigitech@gmail.com>
2025-01-27 05:49:35 +0100

    27/01/2025 05:49

--- includes/form-contact.php
+++ includes/form-contact.php
@@ -1,32 +1,15 @@
 <?php
-echo '<form id="contact-form" method="post" action="" role="form" novalidate>
-    <div class="form-group">
-        <input type="text" name="name" id="name" class="form-field" placeholder="Nom et prénom*" />
-        <p class="comments" id="nameError"></p>
-    </div>
-
-    <div class="form-group">
-        <input type="tel" name="telephone" id="telephone" class="form-field" placeholder="Téléphone*" />
-        <p class="comments" id="telephoneError"></p>
-    </div>
-
-    <div class="form-group">
-        <input type="email" name="email" id="email" class="form-field" placeholder="Email*" />
-        <p class="comments" id="emailError"></p>
-    </div>
-
-    <div class="form-group">
-        <textarea name="message" id="message" class="form-field" cols="30" rows="6" placeholder="Décrivez votre projet*"></textarea>
-        <p class="comments" id="messageError"></p>
-    </div>
-
-    <div class="form-group" id="checkbox-div">
-        <input type="checkbox" id="checkedAccept" name="checkedAccept" />
+echo '<form id="contact-form" action="https://formspree.io/f/xdkozwew" method="POST">
+    <input type="text" name="name" id="name" class="form-field" placeholder="Nom et prénom*" required/>
+        <input type="tel" name="telephone" id="telephone" class="form-field" placeholder="Téléphone*" required/>
+        <input type="email" name="email" id="email" class="form-field" placeholder="Email*" required/>
+        <textarea name="message" id="message" class="form-field" cols="30" rows="6" placeholder="Décrivez votre projet*" required></textarea>
+        <div id="checkbox-div">
+        <input type="checkbox" id="checkedAccept" name="checkedAccept" required/>
         <label for="checkedAccept">
             J’accepte que mes données soient utilisées conformément à la
             <a href="./confidentialite.php">Politique de confidentialité</a>.
         </label>
-        <p class="comments" id="checkedAcceptError"></p>
     </div>
 
     <button type="submit">Envoyer</button>
