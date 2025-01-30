5ab8f8aeca91f2063fb9c669846fb4a8696bf851
Priscilla DigiTech <priscilladigitech@gmail.com>
2025-01-30 13:16:25 +0100

    30/01/2025 13:16

---
+++ includes/galerie-project.php
@@ -0,0 +1,78 @@
+<?php
+
+echo '
+<a href="https://news-mag.webilys.fr/" target="_blank" class="galerie-item">
+<div>
+  <img src="./assets/img/news-mag.png" alt="News Mag" />
+  <p>News Mag</p>
+</div>
+</a>
+<a href="https://voice-converter.webilys.fr/" target="_blank" class="galerie-item">
+<div>
+  <img src="./assets/img/voice-converter.png" alt="Voice Converter" />
+  <p>Voice Converter</p>
+</div>
+</a>
+<a href="https://cryptocurrency.webilys.fr/" target="_blank" class="galerie-item">
+<div>
+  <img src="./assets/img/cryptocurrency.png" alt="Cryptocurrency" />
+  <p>Cryptocurrency</p>
+</div>
+</a>
+<a href="https://qrcode-generator.webilys.fr/" target="_blank" class="galerie-item">
+<div>
+  <img src="./assets/img/qrcode-generator.png" alt="QR Code Generator" />
+  <p>QR Code Generator</p>
+</div>
+</a>
+<a href="https://webilys-images.webilys.fr/" target="_blank" class="galerie-item">
+<div>
+  <img src="./assets/img/webilys-images.png" alt="Webilys Images" />
+  <p>Webilys Images</p>
+</div>
+</a>
+<a href="https://burger-code.webilys.fr/" target="_blank">
+<img src="./assets/img/burger-code.png" alt="Burger Code" />
+</a>
+<a href="https://fo-transport13.webilys.fr/" target="_blank">
+<img src="./assets/img/fo-transport13.png" alt="FO Transport 13" />
+</a>
+<a href="https://lesdelicesdeleana.webilys.fr/" target="_blank">
+<img src="./assets/img/les-delices-de-leana.png" alt="Les delices de Léana" />
+</a>
+<a href="https://danyprovenceherbes.com" target="_blank">
+<img src="./assets/img/dany-provence-herbes.png" alt="Dany Provence Herbes" />
+</a>
+<a href="https://ici-bebe.fr/" target="_blank">
+<img src="./assets/img/ici-bebe.png" alt="Ici Bébé" />
+</a>
+<a href="https://casadusud.fr/" target="_blank">
+<img src="./assets/img/casa-du-sud.png" alt="Casa du sud" />
+</a>
+<a href="https://dpartenr.fr/" target="_blank">
+<img src="./assets/img/dpartenr.png" alt="DPartenR" />
+</a>
+<a href="https://fashiongipsy.fr/" target="_blank">
+<img src="./assets/img/fashion-gipsy.png" alt="" />
+</a>
+<a href="https://institutdelatarasque.fr/" target="_blank">
+<img src="./assets/img/institut-de-la-tarasque.png" alt="Institut de la Tarasque" />
+</a>
+<a href="https://sainte-marthe-tarascon.fr/" target="_blank">
+<img src="./assets/img/sainte-marthe.png" alt="Établissement privé catholique Sainte Marthe" />
+</a>
+<a href="https://jcfamilles.webilys.fr" target="_blank">
+<img src="./assets/img/jcfamilles.png" alt="JC Familles" />
+</a>
+<a href="https://jplecaudey.com/" target="_blank">
+<img src="./assets/img/jp-lecaudey.png" alt="JP Lecaudey" />
+</a>
+<a href="https://7ounet.fr/" target="_blank">
+<img src="./assets/img/7-ou-net.png" alt="7 OU NET" />
+</a>
+<a href="https://the-resto.webilys.fr/" target="_blank">
+<img src="./assets/img/the-resto.png" alt="The resto" />
+</a>
+
+'
+    ?>
--- index.php
+++ index.php
@@ -154,6 +154,12 @@
         </div>
       </div>
       <div class="galerie-container">
+        <a href="https://news-mag.webilys.fr/" target="_blank" class="galerie-item">
+          <div>
+            <img src="./assets/img/news-mag.png" alt="News Mag" />
+            <p>News Mag</p>
+          </div>
+        </a>
         <a href="https://voice-converter.webilys.fr/" target="_blank" class="galerie-item">
           <div>
             <img src="./assets/img/voice-converter.png" alt="Voice Converter" />
